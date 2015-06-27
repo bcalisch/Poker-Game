c4bd4535b27eb388699a05c1a99183559b989571
Michael Boyd <Ucmikeboyd5@gmail.com>
2015-06-27 16:43:19 -0500

    Up to date with all of the hands

--- src/com/pokerGame/Hand.java
+++ src/com/pokerGame/Hand.java
@@ -5,45 +5,55 @@ import java.util.List;
 import java.util.Collections;
 import java.util.ArrayList;
 
+
 /**
  * Created by benjamin on 6/22/15.
  */
-public class Hand{
+public class Hand {
 
     private int highCard1;
     private int highCard2;
     private int highCard3;
     private int highCard4;
     private int highCard5;
-    private String handName = null;
+    private int cardHand;
     private boolean flush = false;
     private List<String> cardsString;
     private List<Integer> cardsInt = new ArrayList<Integer>();
 
-    public Hand(List<String> playerHand){
+
+    public Hand(List<String> playerHand) {
         cardsString = playerHand;
     }
-    public int convertList(){
-         for (int i = 0; i < this.cardsString.size(); i++) {
-            String temp = this.cardsString.get(i);
-            temp = temp.substring(0,1);
 
-            switch(temp){
-                case "T": temp = "10";
+
+
+    public int convertList() {
+        for (int i = 0; i < this.cardsString.size(); i++) {
+            String temp = this.cardsString.get(i);
+            temp = temp.substring(0, 1);
+
+            switch (temp) {
+                case "T":
+                    temp = "10";
                     break;
-                case "J": temp = "11";
+                case "J":
+                    temp = "11";
                     break;
-                case "Q": temp = "12";
+                case "Q":
+                    temp = "12";
                     break;
-                case "K": temp ="13";
+                case "K":
+                    temp = "13";
                     break;
-                case "A": temp = "14";
+                case "A":
+                    temp = "14";
                     break;
             }
 
             Integer holder = Integer.valueOf(temp);
             cardsInt.add(i, holder);
-            Collections.sort(cardsInt);
+            Collections.sort(cardsInt, Collections.reverseOrder());
 
 
         }
@@ -52,8 +62,133 @@ public class Hand{
         return 0;
     }
 
+    public void twoPair() {
+        int size = cardsInt.size() - 1;
+        int counter = 0;
+        int pair1 = 0;
+        int pair2 = 0;
+        boolean onePair = false;
+        boolean twoPair = false;
+
+        for (int i = 0; i < size; i++) {
+            if (cardsInt.get(i) == cardsInt.get(i+1)) {
+                counter++;
+            }
+            if ((counter == 1) && (onePair == false)) {
+                pair1 = cardsInt.get(i);
+                pair2 = pair1;
+                onePair = true;
+            }
+            else if ((counter == 2) && (twoPair == false)) {
+
+                pair2 = cardsInt.get(i);
+                twoPair = true;
+
+            }
+        }
+
+        if (pair1 != pair2 && counter != 3) {
+            int counter2 = 0;
+            int size2 = cardsInt.size() -1;
+            boolean firstHigh = false;
+            boolean secondHigh = false;
+            highCard3 = cardsInt.get(0);
+            for (int i = 0; i < size2; i++) {
+                if (cardsInt.get(i) == cardsInt.get(i+1)) {
+                    counter2++;
+
+                }
+                if ((counter2 == 1) && (firstHigh == false)) {
+                    highCard1 = cardsInt.get(i);
+                    firstHigh = true;
+                    if (highCard3 <= highCard1){
+                        highCard3 = cardsInt.get(i+2);
+                    }
 
 
+                }
+                else if ((counter2 == 2) && (secondHigh == false)) {
+                    highCard2 = cardsInt.get(i);
+                    secondHigh = true;
+                    if (highCard3 <= highCard2){
+                        highCard3 = cardsInt.get(i+2);
+                    }
 
+                }
+            }
+            System.out.println("2Pair only" +" High pair "+ highCard1+ " Second High Pair "+highCard2+ "Third High Paire"+highCard3);
+        } else {
+            System.out.println("Not a 2 pair");
+        }
+
+
+    }
+    public void fullHouse() {
+        int size = cardsInt.size() - 1;
+        int counter = 0;
+        int pair1 = 0;
+        int pair2 = 0;
+        boolean fullHouse = false;
+
+        for (int i = 0; i < size; i++) {
+            if (cardsInt.get(i) == cardsInt.get(i+1)) {
+                counter++;
+            }
+            if (counter == 1) {
+                pair1 = cardsInt.get(i);
+                pair2 = pair1;
+            }
+
+            else if ((counter == 3) && (fullHouse == false)) {
+
+                pair2 = cardsInt.get(i);
+                fullHouse = true;
+
+            }
+        }
+        if (pair1 != pair2 && counter == 3) {
+            highCard1 = cardsInt.get(0);
+            int counter2 =0;
+            boolean fullHouse2 = false;
+            for (int i = 0; i < size; i++) {
+                if (cardsInt.get(i) == cardsInt.get(i+1)) {
+                    counter2++;
+                }
+                if (counter2 == 1) {
+                    highCard1 = cardsInt.get(i);
+                }
+
+                else if ((counter2 == 3) && (fullHouse2 == false)) {
+
+                    highCard2 = cardsInt.get(i);
+                    fullHouse = true;
+
+                }
+            }
+            System.out.println("Full house, with high card 1 is "+highCard1+" And High card 2 is "+highCard2);
+        } else {
+            System.out.println("Not a full House");
+        }
+
+
+    }
+    public void straight() {
+        int size = cardsInt.size() - 1;
+        int counter = 0;
+        int nextCard = 0;
+        highCard1 = cardsInt.get(0);
+        for (int i = 0; i < size; i++) {
+            nextCard = cardsInt.get(i+1)-1;
+            if (cardsInt.get(i) == nextCard) {
+                counter++;
+            }
+
+            }
+        if (counter == 4){
+            System.out.println("You have a straight, with a high card of" + highCard1);
+        }
+        else
+            System.out.println("You do not have a straight");
+        }
 }
 
--- src/com/pokerGame/LoadHands.java
+++ src/com/pokerGame/LoadHands.java
@@ -1,12 +1,12 @@
 package com.pokerGame; /**
  * TODO:
  * Make the following Classes
- * CheckPair-Mike
- * Check2Pair-Benjamin
- * CheckFullHouse-Mike
- * CheckThreeOfAKind-Benjamin
- * CheckFourOfAKind-Mike
- * CheckStraight-Mike
+ * PairCheck-Mike
+ * 2PairCheck-Benjamin
+ * FullHouseCheck-Mike
+ * ThreeOfAKindCheck-Benjamin
+ * FourOfAKindCheck-Mike
+ * StraightCheck-Mike
  * CheckHighCard
  * CheckFlush
  * CheckFinal
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.pokerGame.Hand;
-
+import jdk.internal.util.xml.impl.Pair;
 
 
 public class LoadHands {
@@ -32,77 +32,85 @@ public class LoadHands {
         URL projEU = null;
         List<String> p1 = new ArrayList(Arrays.asList());
         List<String> p2 = new ArrayList(Arrays.asList());
-
-        try {
-            projEU = new URL("https://projecteuler.net/project/resources/p054_poker.txt");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(
-                    new InputStreamReader(
-                            projEU.openStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        //Bit that deals with each line of the file
-        try {
-            while ((inputLine = in.readLine()) != null) {
-               // inputLine.replaceAll(" ", "");
-               // System.out.println(inputLine);
-                //inputLine = inputLine.replaceAll(" ", ",");
-
-
-                /*TODO
-                * make the line of text an array
-                * Separate the array into two arrays (P1 and P2)
-                * Evaluate each hand
-                * If P1 wins, count++
-                * */
-
-
-                //make the line of text an array
-
-                List<String> lineArray = new ArrayList(Arrays.asList(inputLine.split(" ")));
-                p1.clear();
-                p2.clear();
-
-
-
-                //System.out.println(lineArray.get(0));
-               // System.out.println(inputLine);
-                for (int i = 0; i < lineArray.size(); i++){
-                    if (i < 5 ){
-                        p1.add(lineArray.get(i));
-                    }
-                    else {
-                        p2.add(lineArray.get(i));
-
-
-                    }
-
-                }
-                Hand P1 = new Hand(p1);
-                Hand P2 = new Hand(p2);
-
-                System.out.println("p1 = "+ p1);
-                P1.convertList();
-                System.out.println("p2 = " + p2);
-                P2.convertList();
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        List<String> testList = new ArrayList(Arrays.asList("4D","4H","4D","3H","3D"));
+        Hand test = new Hand(testList);
+        test.convertList();
+        test.twoPair();
+        test.fullHouse();
+        test.straight();
+//        try {
+//            projEU = new URL("https://projecteuler.net/project/resources/p054_poker.txt");
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        BufferedReader in = null;
+//        try {
+//            in = new BufferedReader(
+//                    new InputStreamReader(
+//                            projEU.openStream()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        //Bit that deals with each line of the file
+//        try {
+//            while ((inputLine = in.readLine()) != null) {
+//               // inputLine.replaceAll(" ", "");
+//               // System.out.println(inputLine);
+//                //inputLine = inputLine.replaceAll(" ", ",");
+//
+//
+//                /*TODO
+//                * make the line of text an array
+//                * Separate the array into two arrays (P1 and P2)
+//                * Evaluate each hand
+//                * If P1 wins, count++
+//                * */
+//
+//
+//                //make the line of text an array
+//
+//                List<String> lineArray = new ArrayList(Arrays.asList(inputLine.split(" ")));
+//                p1.clear();
+//                p2.clear();
+//
+//
+//
+//                //System.out.println(lineArray.get(0));
+//               // System.out.println(inputLine);
+//                for (int i = 0; i < lineArray.size(); i++){
+//                    if (i < 5 ){
+//                        p1.add(lineArray.get(i));
+//                    }
+//                    else {
+//                        p2.add(lineArray.get(i));
+//
+//
+//                    }
+//
+//                }
+//                Hand P1 = new Hand(p1);
+//                Hand P2 = new Hand(p2);
+//
+//
+//                System.out.println("p1 = "+ p1);
+//                P1.convertList();
+//                P1.twoPair();
+//                System.out.println("p2 = " + p2);
+//                P2.convertList();
+//                P2.twoPair();
+//
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            in.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
